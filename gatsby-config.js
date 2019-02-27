b2d821a3885e9bc3e18528b47928c5fc6d80c722
Rob King <technology@escapecompass.com>
2019-02-26 23:10:02 -0800

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    siteMetadata: {
+        title: `Anna King`,
+        description: `Anna King is a digital marketing specialist for non-profits, and ran in the Victoria 2018 election for city council.`,
+        author: `@annakyyj`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-emotion`,
+            options: {
+                // Accepts all options defined by `babel-plugin-emotion` plugin.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.app/offline
+        // 'gatsby-plugin-offline',
+    ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,156 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Global, css } from '@emotion/core'
+
+import Image from '../components/image'
+import SEO from '../components/seo'
+
+import IconLn from '../images/icon-ln.svg'
+import IconIg from '../images/icon-ig.svg'
+
+const ImageWrapper = styled('div')`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    & .gatsby-image-wrapper {
+        height: 100%;
+        width: 100%;
+    }
+`
+
+const Content = styled('main')`
+    position: relative;
+    z-index: 10;
+    display: grid;
+    padding: 40px 0;
+    grid-gap: 16px;
+    grid-template-rows: auto;
+    grid-template-columns: 16px 1fr 1fr 16px;
+
+    grid-template-areas:
+        '. title title .'
+        '. content content .'
+        '. social social .';
+
+    @media screen and (min-width: 760px) {
+        grid-template-columns: 3% 1fr 1fr 3%;
+        grid-template-areas:
+            '. . title .'
+            '. . content .'
+            '. . social .';
+    }
+`
+
+const Title = styled('h1')`
+    color: rgba(255, 255, 255, 0.8);
+    text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+    font-family: 'Bree', sans-serif;
+    font-size: 62px;
+    text-align: left;
+    margin-bottom: 0;
+    grid-area: title;
+`
+
+const Social = styled('div')`
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    grid-area: social;
+`
+const IconImg = styled('img')`
+    height: 24px;
+    width: 24px;
+    display: block;
+    margin-bottom: 0;
+`
+const Link = styled('a')`
+    width: 36px;
+    height: 36px;
+    border-radius: 50px;
+    margin-left: 8px;
+    margin-right: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 0 #fff;
+    background-color: rgba(255, 255, 255, 0.7);
+    opacity: 0.4;
+    &:hover {
+        opacity: 0.8;
+    }
+`
+
+const ContentBlock = styled('div')`
+    padding: 16px;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 4px;
+    grid-area: content;
+    font-family: 'Futura', sans-serif;
+    font-size: 22px;
+`
+
+const IndexPage = () => {
+    return (
+        <React.Fragment>
+            <Global
+                styles={css`
+                    @font-face {
+                        font-family: 'Bree';
+                        src: url('../fonts/Bree-Bold.woff2') format('woff2'),
+                            url('../fonts/Bree-Bold.woff') format('woff');
+                        font-weight: normal;
+                        font-style: normal;
+                    }
+
+                    @font-face {
+                        font-family: 'Futura';
+                        src: url('../fonts/Futura-CondensedMedium.woff2')
+                                format('woff2'),
+                            url('../fonts/Futura-CondensedMedium.woff')
+                                format('woff');
+                        font-weight: normal;
+                        font-style: normal;
+                    }
+                `}
+            />
+            <SEO title="Home" keywords={[`Anna King`]} />
+            <ImageWrapper>
+                <Image />
+            </ImageWrapper>
+            <Content>
+                <Title>Anna King</Title>
+                <ContentBlock>
+                    <h3>Digital Marketing Strategist</h3>
+                    <p>
+                        Passionate and innovative marketing communications
+                        professional with a specialty in online fundraising,
+                        direct email marketing, and web advertising. Over 15
+                        years of experience in the non-profit industry. Enjoys
+                        collaborative projects with like-minded individuals and
+                        organizations.
+                    </p>
+                    <p>
+                        Lives in Victoria, BC. Wife to Rob King and mother to
+                        daughter, Charlotte and cat, Otto.
+                    </p>
+                </ContentBlock>
+                <Social>
+                    <Link
+                        href="https://www.linkedin.com/in/annakyyj/"
+                        target="_blank"
+                    >
+                        <IconImg src={IconLn} />
+                    </Link>
+                    <Link href="https://twitter.com/annakyyj/" target="_blank">
+                        <IconImg src={IconIg} />
+                    </Link>
+                </Social>
+            </Content>
+        </React.Fragment>
+    )
+}
+
+export default IndexPage
